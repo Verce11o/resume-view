f46a13812cfcd5bc45119c87daf193b87273ecd0
Verce11o <taymas_rashitov@mail.ru>
2024-05-07 19:47:00 +0500

    employee test

---
+++ employee-service/internal/repository/redis/employee_test.go
@@ -0,0 +1,162 @@
+package redis
+
+import (
+	"context"
+	"github.com/Verce11o/resume-view/employee-service/internal/models"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestEmployeeCache_SetEmployee(t *testing.T) {
+
+	ctx := context.Background()
+
+	container, connURI := setupRedisContainer(t)
+	defer container.Terminate(ctx)
+
+	client := redis.NewClient(&redis.Options{
+		Addr: connURI,
+	})
+
+	_, err := client.Ping(ctx).Result()
+
+	require.NoError(t, err)
+
+	employeeID := uuid.New().String()
+	employeeCacheRepo := NewEmployeeCache(client)
+
+	tests := []struct {
+		name       string
+		employeeID string
+		employee   *models.Employee
+		wantErr    bool
+	}{
+		{
+			name:       "Valid input",
+			employeeID: employeeID,
+			employee: &models.Employee{
+				ID:         uuid.New(),
+				FirstName:  "John",
+				LastName:   "Doe",
+				PositionID: uuid.New(),
+				CreatedAt:  time.Now().UTC(),
+				UpdatedAt:  time.Now().UTC(),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := employeeCacheRepo.SetEmployee(ctx, tt.employeeID, tt.employee)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestEmployeeCache_GetEmployee(t *testing.T) {
+	ctx := context.Background()
+
+	container, connURI := setupRedisContainer(t)
+	defer container.Terminate(ctx)
+
+	client := redis.NewClient(&redis.Options{
+		Addr: connURI,
+	})
+
+	employeeCacheRepo := NewEmployeeCache(client)
+
+	employeeID := uuid.New()
+
+	err := employeeCacheRepo.SetEmployee(ctx, employeeID.String(), &models.Employee{
+		ID:        employeeID,
+		FirstName: "John",
+		LastName:  "Doe",
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
+
+	require.NoError(t, err)
+
+	tests := []struct {
+		name       string
+		employeeID string
+		wantErr    bool
+	}{
+		{
+			name:       "Valid input",
+			employeeID: employeeID.String(),
+		},
+		{
+			name:       "Non-existent employee",
+			employeeID: uuid.New().String(),
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := employeeCacheRepo.GetEmployee(ctx, tt.employeeID)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestEmployeeCache_DeleteEmployee(t *testing.T) {
+	ctx := context.Background()
+
+	container, connURI := setupRedisContainer(t)
+	defer container.Terminate(ctx)
+
+	client := redis.NewClient(&redis.Options{
+		Addr: connURI,
+	})
+
+	employeeCacheRepo := NewEmployeeCache(client)
+
+	employeeID := uuid.New()
+
+	err := employeeCacheRepo.SetEmployee(ctx, employeeID.String(), &models.Employee{
+		ID:         employeeID,
+		FirstName:  "John",
+		LastName:   "Doe",
+		PositionID: uuid.New(),
+		CreatedAt:  time.Now().UTC(),
+		UpdatedAt:  time.Now().UTC(),
+	})
+
+	require.NoError(t, err)
+
+	tests := []struct {
+		name       string
+		employeeID string
+		wantErr    bool
+	}{
+		{
+			name:       "Valid input",
+			employeeID: employeeID.String(),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := employeeCacheRepo.DeleteEmployee(ctx, tt.employeeID)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
