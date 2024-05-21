c7d403b438e19c5eefbf94bf88aa6fce5947737b
Verce11o <taymas_rashitov@mail.ru>
2024-05-21 19:40:43 +0500

    employee service tests

--- employee-service/internal/service/employee_test.go
+++ employee-service/internal/service/employee_test.go
@@ -14,6 +14,8 @@ import (
 )
 
 func TestEmployeeService_CreateEmployee(t *testing.T) {
+	t.Parallel()
+
 	type fields struct {
 		employeeRepo *mocks.EmployeeRepository
 		positionRepo *mocks.PositionRepository
@@ -120,11 +122,430 @@ func TestEmployeeService_CreateEmployee(t *testing.T) {
 			}
 
 			employee, err := srv.CreateEmployee(context.TODO(), tt.input)
-			if tt.wantErr {
-				assert.Error(t, err)
-			}
+
+			assert.Equal(t, tt.wantErr, err != nil)
 
 			assert.EqualValues(t, tt.response, employee)
+		})
+	}
+}
+
+func TestEmployeeService_GetEmployee(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		employeeRepo *mocks.EmployeeRepository
+		positionRepo *mocks.PositionRepository
+		cache        *mocks.EmployeeCacheRepository
+	}
+
+	employeeID := uuid.New()
+
+	tests := []struct {
+		name     string
+		id       uuid.UUID
+		response models.Employee
+		mockFunc func(f *fields)
+		wantErr  bool
+	}{
+		{
+			name: "Valid return from cache",
+			id:   employeeID,
+			response: models.Employee{
+				ID:        employeeID,
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+			mockFunc: func(f *fields) {
+				f.cache.On("GetEmployee", mock.Anything, mock.AnythingOfType("string")).
+					Return(&models.Employee{
+						ID:        employeeID,
+						FirstName: "John",
+						LastName:  "Doe",
+					}, nil)
+			},
+		},
+		{
+			name: "Valid return from repo",
+			id:   employeeID,
+			response: models.Employee{
+				ID:        employeeID,
+				FirstName: "John",
+				LastName:  "Doe",
+			},
+			mockFunc: func(f *fields) {
+				f.cache.On("GetEmployee", mock.Anything, mock.AnythingOfType("string")).
+					Return(nil, assert.AnError)
+
+				f.employeeRepo.On("GetEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(models.Employee{
+						ID:        employeeID,
+						FirstName: "John",
+						LastName:  "Doe",
+					}, nil)
+
+				f.cache.On("SetEmployee", mock.Anything, mock.AnythingOfType("string"),
+					mock.AnythingOfType("*models.Employee")).
+					Return(nil)
+			},
+		},
+		{
+			name:     "Invalid employee ID",
+			id:       uuid.Nil,
+			response: models.Employee{},
+			mockFunc: func(f *fields) {
+				f.cache.On("GetEmployee", mock.Anything, mock.AnythingOfType("string")).
+					Return(nil, assert.AnError)
+
+				f.employeeRepo.On("GetEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(models.Employee{}, assert.AnError)
+			},
+
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			employeeRepo := mocks.NewEmployeeRepository(t)
+			positionRepo := mocks.NewPositionRepository(t)
+			transactor := mocks.NewTransactor(t)
+			cache := mocks.NewEmployeeCacheRepository(t)
+
+			tt.mockFunc(&fields{
+				employeeRepo: employeeRepo,
+				positionRepo: positionRepo,
+				cache:        cache,
+			})
+
+			srv := &EmployeeService{
+				log:          zap.NewNop().Sugar(),
+				employeeRepo: employeeRepo,
+				positionRepo: positionRepo,
+				cache:        cache,
+				transactor:   transactor,
+			}
+
+			employee, err := srv.GetEmployee(context.TODO(), tt.id)
+
+			assert.Equal(t, tt.wantErr, err != nil)
+
+			assert.EqualValues(t, tt.response, employee)
+		})
+	}
+}
+
+func TestEmployeeService_GetEmployeeList(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		employeeRepo *mocks.EmployeeRepository
+		positionRepo *mocks.PositionRepository
+		cache        *mocks.EmployeeCacheRepository
+	}
+
+	employeeID := uuid.New()
+
+	tests := []struct {
+		name     string
+		cursor   string
+		response models.EmployeeList
+		mockFunc func(f *fields)
+		wantErr  bool
+	}{
+		{
+			name:   "Valid empty cursor",
+			cursor: "",
+			response: models.EmployeeList{
+				Cursor: "cursorExample",
+				Employees: []models.Employee{
+					{
+						ID:        employeeID,
+						FirstName: "John",
+						LastName:  "Doe",
+					},
+				},
+			},
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("GetEmployeeList", mock.Anything, mock.AnythingOfType("string")).
+					Return(models.EmployeeList{
+						Cursor: "cursorExample",
+						Employees: []models.Employee{
+							{
+								ID:        employeeID,
+								FirstName: "John",
+								LastName:  "Doe",
+							},
+						},
+					}, nil)
+			},
+		},
+		{
+			name:     "Invalid cursor",
+			cursor:   "invalid",
+			response: models.EmployeeList{},
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("GetEmployeeList", mock.Anything, mock.AnythingOfType("string")).
+					Return(models.EmployeeList{}, assert.AnError)
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			employeeRepo := mocks.NewEmployeeRepository(t)
+			positionRepo := mocks.NewPositionRepository(t)
+			transactor := mocks.NewTransactor(t)
+			cache := mocks.NewEmployeeCacheRepository(t)
+
+			tt.mockFunc(&fields{
+				employeeRepo: employeeRepo,
+				positionRepo: positionRepo,
+				cache:        cache,
+			})
+
+			srv := &EmployeeService{
+				log:          zap.NewNop().Sugar(),
+				employeeRepo: employeeRepo,
+				positionRepo: positionRepo,
+				cache:        cache,
+				transactor:   transactor,
+			}
+
+			employee, err := srv.GetEmployeeList(context.TODO(), tt.cursor)
+
+			assert.Equal(t, tt.wantErr, err != nil)
+
+			assert.EqualValues(t, tt.response, employee)
+		})
+	}
+}
+
+func TestEmployeeService_UpdateEmployee(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		employeeRepo *mocks.EmployeeRepository
+		positionRepo *mocks.PositionRepository
+		cache        *mocks.EmployeeCacheRepository
+	}
+
+	employeeID := uuid.New()
+	positionID := uuid.New()
+
+	tests := []struct {
+		name     string
+		input    domain.UpdateEmployee
+		response models.Employee
+		mockFunc func(f *fields)
+		wantErr  bool
+	}{
+		{
+			name: "Valid input",
+			input: domain.UpdateEmployee{
+				EmployeeID: employeeID,
+				PositionID: positionID,
+				FirstName:  "John",
+				LastName:   "Doe",
+				Salary:     30999,
+			},
+			response: models.Employee{
+				ID:         employeeID,
+				FirstName:  "John",
+				LastName:   "Doe",
+				PositionID: positionID,
+			},
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("UpdateEmployee", mock.Anything, mock.AnythingOfType("domain.UpdateEmployee")).
+					Return(models.Employee{
+						ID:         employeeID,
+						FirstName:  "John",
+						LastName:   "Doe",
+						PositionID: positionID,
+					}, nil)
+
+				f.cache.On("DeleteEmployee", mock.Anything, mock.AnythingOfType("string")).
+					Return(nil)
+			},
+		},
+		{
+			name: "Invalid input",
+			input: domain.UpdateEmployee{
+				EmployeeID: employeeID,
+				PositionID: uuid.Nil,
+				FirstName:  "John",
+				LastName:   "Doe",
+				Salary:     30999,
+			},
+			response: models.Employee{},
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("UpdateEmployee", mock.Anything, mock.AnythingOfType("domain.UpdateEmployee")).
+					Return(models.Employee{}, assert.AnError)
+			},
+			wantErr: true,
+		},
+		{
+			name: "Cache error",
+			input: domain.UpdateEmployee{
+				EmployeeID: employeeID,
+				PositionID: positionID,
+				FirstName:  "John",
+				LastName:   "Doe",
+				Salary:     30999,
+			},
+			response: models.Employee{
+				ID:         employeeID,
+				FirstName:  "John",
+				LastName:   "Doe",
+				PositionID: positionID,
+			},
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("UpdateEmployee", mock.Anything, mock.AnythingOfType("domain.UpdateEmployee")).
+					Return(models.Employee{
+						ID:         employeeID,
+						FirstName:  "John",
+						LastName:   "Doe",
+						PositionID: positionID,
+					}, nil)
+
+				f.cache.On("DeleteEmployee", mock.Anything, mock.AnythingOfType("string")).
+					Return(assert.AnError)
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			employeeRepo := mocks.NewEmployeeRepository(t)
+			positionRepo := mocks.NewPositionRepository(t)
+			transactor := mocks.NewTransactor(t)
+			cache := mocks.NewEmployeeCacheRepository(t)
+			tt.mockFunc(&fields{
+				employeeRepo: employeeRepo,
+				positionRepo: positionRepo,
+				cache:        cache,
+			})
+
+			srv := &EmployeeService{
+				log:          zap.NewNop().Sugar(),
+				employeeRepo: employeeRepo,
+				positionRepo: positionRepo,
+				cache:        cache,
+				transactor:   transactor,
+			}
+			employee, err := srv.UpdateEmployee(context.TODO(), tt.input)
+
+			assert.Equal(t, tt.wantErr, err != nil)
+
+			assert.EqualValues(t, tt.response, employee)
+		})
+	}
+}
+
+func TestEmployeeService_DeleteEmployee(t *testing.T) {
+	t.Parallel()
+
+	type fields struct {
+		employeeRepo *mocks.EmployeeRepository
+		positionRepo *mocks.PositionRepository
+		cache        *mocks.EmployeeCacheRepository
+	}
+
+	employeeID := uuid.New()
+	positionID := uuid.New()
+
+	tests := []struct {
+		name     string
+		id       uuid.UUID
+		mockFunc func(f *fields)
+		wantErr  bool
+	}{
+		{
+			name: "Valid input",
+			id:   employeeID,
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("GetEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(models.Employee{
+						ID:         employeeID,
+						FirstName:  "John",
+						LastName:   "Doe",
+						PositionID: positionID,
+					}, nil)
+
+				f.employeeRepo.On("DeleteEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(nil)
+
+				f.cache.On("DeleteEmployee", mock.Anything, mock.AnythingOfType("string")).
+					Return(nil)
+			},
+		},
+		{
+			name: "Non-existing employee",
+			id:   uuid.New(),
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("GetEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(models.Employee{}, assert.AnError)
+			},
+			wantErr: true,
+		},
+		{
+			name: "Repository error",
+			id:   employeeID,
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("GetEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(models.Employee{
+						ID:        employeeID,
+						FirstName: "John",
+						LastName:  "Doe",
+					}, nil)
+
+				f.employeeRepo.On("DeleteEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(assert.AnError)
+			},
+			wantErr: true,
+		},
+		{
+			name: "Cache error",
+			id:   employeeID,
+			mockFunc: func(f *fields) {
+				f.employeeRepo.On("GetEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(models.Employee{
+						ID:        employeeID,
+						FirstName: "John",
+						LastName:  "Doe",
+					}, nil)
+
+				f.employeeRepo.On("DeleteEmployee", mock.Anything, mock.AnythingOfType("uuid.UUID")).
+					Return(nil)
+
+				f.cache.On("DeleteEmployee", mock.Anything, mock.AnythingOfType("string")).
+					Return(assert.AnError)
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			employeeRepo := mocks.NewEmployeeRepository(t)
+			positionRepo := mocks.NewPositionRepository(t)
+			transactor := mocks.NewTransactor(t)
+			cache := mocks.NewEmployeeCacheRepository(t)
+			tt.mockFunc(&fields{
+				employeeRepo: employeeRepo,
+				positionRepo: positionRepo,
+				cache:        cache,
+			})
+
+			srv := &EmployeeService{
+				log:          zap.NewNop().Sugar(),
+				employeeRepo: employeeRepo,
+				positionRepo: positionRepo,
+				cache:        cache,
+				transactor:   transactor,
+			}
+
+			err := srv.DeleteEmployee(context.TODO(), tt.id)
+			assert.Equal(t, tt.wantErr, err != nil)
 		})
 	}
 }
